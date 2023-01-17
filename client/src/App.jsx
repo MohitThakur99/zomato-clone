673968b6aca4a4b285889589030e1c8f5151ddae
MohitThakur99 <mohitthakurji0@gmail.com>
2023-01-17 21:22:09 +0530

    add navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
+// HOC
+import HomeLayoutHOC from "./HOC/Home.hoc";
+
+// components
+import Temp from "./components/Temp";
 
 function App() {
   return (
     <>
-    <h1 className="text-blue-700">Hello tailwind</h1>
+      <HomeLayoutHOC path="/" exact component={Temp} />
     </>
   );
 }
