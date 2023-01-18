aa269068a8577f531e89137095674a0f566a3a48
MohitThakur99 <mohitthakurji0@gmail.com>
2023-01-18 13:11:34 +0530

    add active food tabs mobile screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import HomeLayoutHOC from "./HOC/Home.hoc";
 
 // components
 import Temp from "./components/Temp";
+import Master from "./components/master";
 
 function App() {
   return (
     <>
       <HomeLayoutHOC path="/" exact component={Temp} />
+      <HomeLayoutHOC path="/:type" exact component={Master} />
     </>
   );
 }
